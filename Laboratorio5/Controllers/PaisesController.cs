d55d06038abb22419d267d916d37ab8d47339a3d
Diego Chinchilla <diego.chinchilla98@gmail.com>
2022-05-21 17:22:59 -0600

    Laboratorio #6: Creacion de país terminada

--- Laboratorio5/Controllers/PaisesController.cs
+++ Laboratorio5/Controllers/PaisesController.cs
@@ -1,4 +1,5 @@
 ﻿using Laboratorio5.Handlers;
+using Laboratorio5.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Laboratorio5.Controllers
@@ -11,6 +12,38 @@ namespace Laboratorio5.Controllers
             var paises = paisesHandler.ObtenerPaises();
             ViewBag.MainTitle = "Lista de países";
             return View(paises);
+        }
+
+        [HttpGet]
+        public IActionResult CrearPais()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CrearPais(PaisModel pais)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PaisesHandler paisesHandler = new PaisesHandler();
+                    ViewBag.ExitoAlCrear = paisesHandler.CrearPais(pais);
+
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El país" + "" + pais.Nombre + "fue creado con éxito";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salió mal y no fue posible crear el país";
+                return View();
+            }
         }
     }
 }
---
+++ Laboratorio5/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE Pais(
+Id INTEGER PRIMARY KEY IDENTITY,
+Nombre VARCHAR(200) NOT NULL,
+Idioma VARCHAR(200) NOT NULL,
+Continente NVARCHAR(100) NOT NULL
+)
+GO
+
+INSERT INTO [dbo].[Pais] ([Nombre],[Idioma] ,[Continente])
+VALUES('Costa Rica', 'Español', 'América'),
+('Argentina', 'Español', 'América'),
+('Canada', 'Inglés/Frances', 'América'),
+('Francia', 'Frances', 'Europa'),
+('España', 'Español', 'Europa')
+GO
+
+Select * From Pais
